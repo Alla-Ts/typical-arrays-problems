781145acab12abf100fed94b9edf55e557764022
Alla-Ts <aecybko@gmail.com>
2020-10-02 20:53:18 +0300

    fix: test passed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    if (array == null || array.length == 0) {
+        return 0;
+    }
+    return Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+
+    if (array == null || array.length == 0) {
+        return 0;
+    }
+    return Math.max.apply(null, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array == null || array.length == 0) {
+        return 0;
+    }
+    let sumNum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sumNum += array[i]
+    }
+    return (sumNum / array.length);
 }
